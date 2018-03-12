1e88a8d86eea21bfbaf94c827c21441b3346cf22
npolyak <nick.polyak@verizon.net>
2018-03-12 18:46:42 -0400

    added DefaultWrapperAttribute

---
+++ Attributes/DefaultImplAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class DefaultImplAttribute : Attribute
+    {
+        public Type ImplType { get; }
+
+        public DefaultImplAttribute(Type implType)
+        {
+            this.ImplType = implType;
+        }
+    }
+}
