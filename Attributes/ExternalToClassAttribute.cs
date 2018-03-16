f0ca2d531bbd21c36d665175b1baddff7ea07d75
npolyak <nick.polyak@verizon.net>
2018-03-16 14:21:05 -0400

    added a new class

---
+++ Attributes/ExternalToClassAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Utilities.Attributes
+{
+    // ExternalToClass means that it is not produced by the class
+    // it is produced externally to the class and then set to the property.
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class ExternalToClassAttribute : Attribute
+    {
+    }
+}
