a9d68aa36c2c3bfd7007d5f0853447bd0f139aba
npolyak <nick.polyak@verizon.net>
2019-05-19 15:21:44 -0400

    added some missing files.

---
+++ XmlSerializationUtils.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NP.Utilities
+{
+    public static class XmlSerializationUtils
+    {
+        public static string Serialize<T>(this T objToSerialize)
+        {
+            ISaveable saveable = objToSerialize as ISaveable;
+
+            if (saveable != null)
+            {
+                saveable.BeforeSave();
+            }
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = UTF8Encoding.ASCII;
+            settings.Indent = true;
+            settings.NewLineChars = "\n";
+            settings.NewLineOnAttributes = true;
+            settings.IndentChars = "    ";
+            settings.CloseOutput = true;
+
+            using (XmlWriter xmlWriter = XmlWriter.Create(stringBuilder, settings))
+            {
+                xmlSerializer.Serialize(xmlWriter, objToSerialize);
+            }
+
+            return stringBuilder.ToString().ReplaceEncoding();
+        }
+
+        public static T Deserialize<T>(string xmlStr)
+        {
+            if (xmlStr.IsNullOrEmpty())
+            {
+                return default(T);
+            }
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            using (StringReader stringReader = new StringReader(xmlStr))
+            {
+                T result = (T)xmlSerializer.Deserialize(stringReader);
+
+                IRestorable restorable =
+                    result as IRestorable;
+                if (restorable != null)
+                {
+                    restorable.AfterRestore();
+                }
+
+                return result;
+            }
+        }
+
+        static string ReplaceEncoding(this string str)
+        {
+            return str.Replace
+            (
+                "<?xml version=\"1.0\" encoding=\"utf-16\"?>",
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+        }
+    }
+}
