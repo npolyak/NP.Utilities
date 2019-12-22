7d43bf880d0086e3036589c83c1592f72789f181
npolyak <nick.polyak@verizon.net>
2019-12-22 15:16:32 -0500

    added a file that should have been added before

---
+++ Attributes/CompositeConstructorAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NP.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    public class CompositeConstructorAttribute : Attribute
+    {
+    }
+}
