ab1e294300023a7f63aa07b345e9720084937cfa
npolyak <nick.polyak@verizon.net>
2021-09-24 18:09:14 -0400

    added ValueContainer.cs

---
+++ src/ValueContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace NP.Utilities
+{
+    public class ValueContainer<T>
+    {
+        public T Value { get; }
+
+        public ValueContainer(T value)
+        {
+            Value = value;
+        }
+    }
+}
