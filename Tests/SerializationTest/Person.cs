42b5fe66201f7ffd654a5b68ecaaf69c834cbb7f
npolyak <nick.polyak@verizon.net>
2022-03-14 19:09:06 -0400

    checkpoint

---
+++ Tests/SerializationTest/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace SerializationTest
+{
+    public class Person
+    {
+        [XmlAttribute]
+        public int Age { get; set; }
+
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"age = {Age}, name = {Name}";
+        }
+    }
+}
---
+++ Tests/SerializationTest/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NP.Utilities;
+using SerializationTest;
+
+Person p = new Person() { Name = "Joe", Age = 20 };
+
+p.SerializeToFile("MyFile.xml");
+
+Person p1 = XmlSerializationUtils.DeserializeFromFile<Person>("MyFile.xml", true);
+
+Console.WriteLine(p1);
