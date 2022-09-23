6505d3bc6c6a3e8e21c843e8eeb3ead3188d8db6
npolyak <nick.polyak@verizon.net>
2022-09-23 16:36:45 -0400

    added some files.

---
+++ src/Attributes/TriggerSourceAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NP.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public class TriggerSourceAttribute : Attribute
+    {
+    }
+}
