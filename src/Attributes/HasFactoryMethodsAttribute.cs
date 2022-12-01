f66bb890735f36e4081b3d18a5cc63a1f1d7df2a
npolyak <nick.polyak@verizon.net>
2022-11-30 22:38:29 -0500

    factored the IoC attributes out into a different repo

--- src/Attributes/HasFactoryMethodsAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NP.Utilities.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-    public class HasFactoryMethodsAttribute : Attribute
-    {
-    }
-}
--- src/Attributes/PluginAssemblyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NP.Utilities.Attributes
-{
-    [AttributeUsage(AttributeTargets.Assembly)]
-    public class PluginAssemblyAttribute : Attribute
-    {
-    }
-}
