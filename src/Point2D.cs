82caf9d68f9f50b6519d920da297a6d75aabaa03
npolyak <nick.polyak@verizon.net>
2026-05-08 16:59:44 -0400

    added ability to set Point2D in initializers and other minor changes

--- src/Point2D.cs
+++ src/Point2D.cs
@@ -21,10 +21,10 @@ namespace NP.Utilities
         where T : IComparable<T>
     {
         [XmlAttribute]
-        public T X { get; }
+        public T X { get; init; }
 
         [XmlAttribute]
-        public T Y { get; }
+        public T Y { get; init; }
 
         public Point2D()
         {
